73c2136d5ba59c98671b360f3ad27e8a86333d87
Raghav893 <raghavarora2309@gmail.com>
2026-04-03 12:08:25 +0530

    restarting cart service

--- CartService/src/main/java/com/raghav/cartservice/common/security/AuthenticatedUser.java
+++
@@ -1,6 +0,0 @@
-package com.raghav.cartservice.common.security;
-
-import java.util.UUID;
-
-public record AuthenticatedUser(UUID userId, String username) {
-}
--- UserService/src/main/java/com/raghav/userservice/common/OpenApiConfig.java
+++ UserService/src/main/java/com/raghav/userservice/common/OpenApiConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(title = "Xclone API", version = "v1"),
+        info = @Info(title = "", version = "v1"),
         security = @SecurityRequirement(name = "bearerAuth")
 )
 @SecurityScheme(
